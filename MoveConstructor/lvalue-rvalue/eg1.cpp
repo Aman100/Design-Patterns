fb33ce45654439461f8a3b3acbb4770543dddd33
Aman Warudkar <31200413+Aman100@users.noreply.github.com>
2024-03-25 13:26:36 -0700

    Update eg1.cpp

--- MoveConstructor/lvalue-rvalue/eg1.cpp
+++ MoveConstructor/lvalue-rvalue/eg1.cpp
@@ -6,11 +6,11 @@ rvalue: which is not lvalue. These values will be stored in registers (CPU memor
 for calculation purpose.
 
 Line 1: int x=20, y=30;	//20,40 will be stored in registers (rvalue)
-Line 2: int z=x*y;	/* value of x which is 20 will be stored in register, 
+Line 2: int z=x*y;	 value of x which is 20 will be stored in register, 
 			   value of y which 30 will be stored in register then operation
 			   will be performed. It means that lvalue will be converted to
 			   rvalue for calculation purpose then final value which is 50
-			   will be assigned to lvalue(z) */
+			   will be assigned to lvalue(z) 
 
 rvalue does not have memory location.
 */
